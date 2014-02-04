a6744492a6ff1ea4dd8feb1df09d8e45c9451189
Matt Oquist <moquist@majen.net>
2014-02-04 08:43:29 -0500

    Work so far.. Even though we are not continusing with John-the-ripper

--- johns.py
+++ johns.py
@@ -12,7 +12,7 @@ import signal
 
 
 # Run john-the-ripper in child process, then terminate child
-def thread(pw,user_count):
+def thread(pw,user_count,pipe_write):
     john_path = '/home/jdellaire/john-the-ripper%d/run/john' %user_count
     file_name = '/tmp/pw%d' % user_count
     process_name = shlex.split(john_path + ' ' + file_name)
@@ -23,17 +23,17 @@ def thread(pw,user_count):
     tpid = os.fork()
     # Child process
     if tpid == 0:
-        time = timer(proc.pid)
+        time = timer(proc.pid,pipe_write)
+        pipe_write = os.fdopen(pipe_write,'w')
+        pipe_write.write(str(user_count))
+        pipe_write.close()
         print "Total time for %d is %d" %(proc.pid,time)
-        os._exit(0)
     # Parent process
     else:
-        # Use the following to terminate subprocess
-        #print "proc.pid = %s" %proc.pid
-        #os.kill(proc.pid,signal.SIGKILL)
         proc.wait()
         os.remove(file_name)
-        os._exit(0)
+
+    os._exit(0)
 
 # Test if child process is still active - pid array input
 def check_multi_pid(pid_arr,thread_count):
@@ -56,14 +56,14 @@ def check_single_pid(pid):
         return True
 
 # Timer function
-def timer(pid):
+def timer(pid,pipe_write):
     start_time = time.time()
     while check_single_pid(pid):
         ctime = time.time() - start_time
         if (ctime > 10):
-            print "Process %d is > 10 seconds.  Exiting.." %pid
+            print "Process %d is >= 10 seconds.  Exiting.." %pid
             os.kill(pid,signal.SIGKILL)
-            break
+            return ctime
     end_time = time.time()
 
     return (end_time - start_time)
@@ -72,11 +72,13 @@ def timer(pid):
 # Main function
 def main():
     # Main variables
+    # TODO: Allow user to specify max thread count (Max of 8?)
     data = {}
-    max_thread = 8
+    max_thread = 1
     thread_count = 0
     user_count = 0
     pid_arr = []
+    bad_pw = []
 
 
     # Build dictionary of passwords and users
@@ -88,6 +90,7 @@ def main():
         data[password]['usernames'].append(username)
 
     pw_count = len(data)
+    pipe_read, pipe_write = os.pipe()
 
     # Create up to max_thread processes
     while (user_count < pw_count):
@@ -98,7 +101,9 @@ def main():
             if pid >= 0:
                 # Child process
                 if pid == 0:
-                    thread(data.keys()[user_count-1],user_count)
+                    os.close(pipe_read)
+                    #print data.values()[user_count-1]
+                    thread(data.keys()[user_count-1],user_count,pipe_write)
                 # Parent process
                 else:
                     pid_arr.append(pid)
@@ -108,9 +113,16 @@ def main():
         elif thread_count == max_thread:
             thread_count, pid_arr = check_multi_pid(pid_arr,thread_count)
 
+    os.close(pipe_write)
+    pipe_read = os.fdopen(pipe_read)
+
     while pid_arr:
+        bad_pw.append(pipe_read.read())
         thread_count, pid_arr = check_multi_pid(pid_arr,thread_count)
 
+    pipe_read.close()
+
+    print "Bad pw users: *%s*" %bad_pw
 
 if __name__ == "__main__":
     main()
